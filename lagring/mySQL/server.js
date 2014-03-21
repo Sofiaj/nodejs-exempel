86126675fc83e76ce39a2420fab22202d1af3518
Niklas Ekman <niklas.ekman@gymnasium.ax>
2014-03-21 13:06:18 +0000

    Lagra i mySQL 02

--- lagring/mySQL/server.js
+++ lagring/mySQL/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var mysql = require('mysql');
 
-var db = mysql.createConnection({
+var databas = mysql.createConnection({
   host:     '127.0.0.1',
   user:     'counter',
   password: 'counter',
@@ -11,10 +11,29 @@ var db = mysql.createConnection({
 var counter = 0;
 
 var server = http.createServer(function(req, res) {
-  counter++;
+  
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.write('Jag har visats ' + counter + ' gånger.');
-  res.end();
+  
+  databas.query(
+    "INSERT INTO `counter`.`counts` (`id`, `date`) VALUES (NULL, CURRENT_DATE());",
+    function(error) {
+      if (error) throw error;
+    }
+  );
+  
+  databas.query(
+    "SELECT * FROM `counts`",
+    function(error, resultat) {
+      
+      if (error) throw error;
+      
+      counter = resultat.length;
+      
+      res.write('Jag har visats ' + counter + ' gånger.');
+      res.end();
+    }
+  );
+  
 });
 
 server.listen(4000);
